c2feb8e173d2a2125c170ea561e7e376be144417
costya <costap@crow.co.il>
2017-10-10 19:13:07 +0300

    Language, home, news ok. + Map info

--- client/app/components/hotels/hotels.controller.js
+++ client/app/components/hotels/hotels.controller.js
@@ -1,5 +1,5 @@
 class HotelsController {
-  constructor(hotelsService, languageFactory, $timeout) {
+  constructor(hotelsService, languageFactory, NgMap, $timeout) {
     "ngInject";
     this.name = 'hotels';
     this.$timeout = $timeout;
@@ -8,6 +8,7 @@ class HotelsController {
     this.currentPost = "Loading...";
     this.posts = [];
     this.locations = [];
+    NgMap.getMap().then(map => this.map = map);
     this.service.loadHotels(this.lng.getLanguage())
       .then(hotels => {
         console.log("Hotels: ", hotels);
@@ -15,6 +16,29 @@ class HotelsController {
         this.posts = hotels.posts;
         this.locations = this.posts.filter(p => p.custom_fields.location && p.custom_fields.location.length);
       });
+
+    this.showInfo = this._showInfo.bind(this);
+  }
+
+  setCurrentContent(item) {
+    let el = $('<div>' + item.content + '</div>');
+    el.find('.b-wrapper').remove();
+    el.find('script').remove();
+    item.content = el.html();
+
+    this.post = item;
+    this.$timeout(() => {
+      let ns = $('.info').getNiceScroll(0);
+      ns.doScrollTop(0, 0);
+      ns.resize();
+    }, 500);
+
+  }
+
+  _showInfo(e, item) {
+    this.setCurrentContent(item);
+    console.log("Show info", this);
+    this.map.showInfoWindow('hotel-info', "custom-marker-"+item.id);
   }
 }
 
